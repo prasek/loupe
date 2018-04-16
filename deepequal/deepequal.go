41b7d470ecb0e665016d9d1149e9272b09090cd6
Phil Prasek <prasek@gmail.com>
2018-04-16 12:06:00 -0700

    split out diff func

--- deepequal/deepequal.go
+++ deepequal/deepequal.go
@@ -28,6 +28,95 @@ func Require(t *testing.T, exp, act interface{}, format string, args ...interfac
 	deepEqual(t, exp, act, false, format, args...)
 }
 
+// returns a color code diff string
+// grab is the number of context lines before and after the diff to grab
+func Diff(exp, act interface{}, grab int) string {
+	//min grab
+	if grab < 0 {
+		grab = 0
+	}
+
+	//adjust pointer to value if needed for compare
+	expt := reflect.TypeOf(exp)
+	if expt.Kind() == reflect.Ptr {
+		expv := reflect.ValueOf(exp)
+		exp = expv.Elem().Interface()
+	}
+
+	actt := reflect.TypeOf(act)
+	if actt.Kind() == reflect.Ptr {
+		actv := reflect.ValueOf(act)
+		act = actv.Elem().Interface()
+	}
+
+	// diff output
+	d := gdiff.New()
+	expText := fmt.Sprintf("%s", exp)
+	actText := fmt.Sprintf("%s", act)
+	diffs := d.DiffMain(expText, actText, false)
+
+	var top bytes.Buffer
+	var bottom bytes.Buffer
+	var out bytes.Buffer
+
+	for _, diff := range diffs {
+		switch diff.Type {
+		case gdiff.DiffDelete:
+			if top.Len() > 0 {
+				out.WriteString(top.String())
+				top.Reset()
+			}
+			if bottom.Len() > 0 {
+				out.WriteString(fmt.Sprintf("\n\n...\n\n"))
+				out.WriteString(bottom.String())
+				bottom.Reset()
+			}
+
+			out.WriteString(red.Sprintf("%s", diff.Text))
+
+		case gdiff.DiffInsert:
+			if top.Len() > 0 {
+				out.WriteString(top.String())
+				top.Reset()
+			}
+			if bottom.Len() > 0 {
+				out.WriteString(fmt.Sprintf("\n\n...\n\n"))
+				out.WriteString(bottom.String())
+				bottom.Reset()
+			}
+
+			out.WriteString(green.Sprintf("%s", diff.Text))
+
+		case gdiff.DiffEqual:
+			lines := strings.Split(diff.Text, "\n")
+
+			if len(lines) <= 2*grab {
+				top.WriteString(diff.Text)
+			} else {
+				top.WriteString(strings.Join(lines[:grab], "\n"))
+				bottom.WriteString(strings.Join(lines[len(lines)-grab:], "\n"))
+			}
+
+		default:
+			out.WriteString(fmt.Sprintf("Unknown diff type: %v", diff.Type))
+			break
+		}
+	}
+
+	if top.Len() > 0 {
+		lines := strings.Split(top.String(), "\n")
+
+		if len(lines) <= grab {
+			out.WriteString(top.String())
+		} else {
+			out.WriteString(strings.Join(lines[:grab], "\n"))
+		}
+	}
+
+	return out.String()
+
+}
+
 // verifies the underlying instances are equal with diff output
 func deepEqual(t *testing.T, exp, act interface{}, assert bool, format string, args ...interface{}) {
 
@@ -56,63 +145,7 @@ func deepEqual(t *testing.T, exp, act interface{}, assert bool, format string, a
 		red.Printf("%s:%d: Not Equal (%T/%T)\n%s\n", base, ln, exp, act, msg)
 		red.Println(line)
 
-		// diff output
-		d := gdiff.New()
-		expText := fmt.Sprintf("%s", exp)
-		actText := fmt.Sprintf("%s", act)
-		diffs := d.DiffMain(expText, actText, false)
-
-		var top bytes.Buffer
-		var bottom bytes.Buffer
-
-		for _, diff := range diffs {
-			switch diff.Type {
-			case gdiff.DiffDelete:
-				if top.Len() > 0 {
-					fmt.Printf(top.String())
-					top.Reset()
-				}
-				if bottom.Len() > 0 {
-					fmt.Printf("\n\n...\n\n")
-					fmt.Printf(bottom.String())
-					bottom.Reset()
-				}
-
-				red.Printf("%s", diff.Text)
-
-			case gdiff.DiffInsert:
-				if top.Len() > 0 {
-					fmt.Printf(top.String())
-					top.Reset()
-				}
-				if bottom.Len() > 0 {
-					fmt.Printf("\n\n...\n\n")
-					fmt.Printf(bottom.String())
-					bottom.Reset()
-				}
-
-				green.Printf("%s", diff.Text)
-
-			case gdiff.DiffEqual:
-				grab := 10
-				lines := strings.Split(diff.Text, "\n")
-
-				if len(lines) <= 2*grab {
-					top.WriteString(diff.Text)
-				} else {
-					top.WriteString(strings.Join(lines[:grab], "\n"))
-					bottom.WriteString(strings.Join(lines[len(lines)-grab:], "\n"))
-				}
-
-			default:
-				fmt.Printf("Unknown diff type: %v", diff.Type)
-				break
-			}
-		}
-
-		if top.Len() > 0 {
-			fmt.Println(top.String())
-		}
+		//out
 
 		if assert {
 			t.FailNow()
